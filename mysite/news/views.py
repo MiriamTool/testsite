1598c67136da7b95ef3f208dd7be4c52a9b5c1f4
MiriamTool <G483@mail.ru>
2022-11-09 19:43:25 +0300

    Changed add_news function in favor of CreateNews class, which is based upon CreateView class from django.view.generic

--- mysite/news/views.py
+++ mysite/news/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404, redirect
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, CreateView
+from django.urls import reverse_lazy
 
 from .models import News, Category
 from .forms import NewsForm, DivErrorList
@@ -39,6 +40,12 @@ class ViewNews(DetailView):
     # pk_url_kwarg = 'news_id'
     # template_name = 'news/news_detail.html' #дефолтное значение имени темплейта *_detail
 
+class CreateNews(CreateView):
+    form_class = NewsForm
+
+    template_name = 'news/add_news.html'
+    # success_url = reverse_lazy('home') #по дефолту при успешном выполнении, редирект происходит на ссылку, которая автоматически получается методом get_absolute_url модели, но можно прописать свой путь
+
 
 # def index(request):
 #     print(request)
@@ -59,14 +66,14 @@ def get_category(request, category_id):
 #     news_item = get_object_or_404(News, pk=news_id) #применение функции для получения объекта либо вывода 404 ошибки вместо 500 ошибки (ошибка сервера)
 #     return render(request, 'news/view_news.html', {'news_item': news_item})
 
-def add_news(request):
-    if request.method == 'POST':
-        form = NewsForm(request.POST, error_class=DivErrorList)
-        if form.is_valid():
-            # print(form.cleaned_data)
-            # news = News.objects.create(**form.cleaned_data) #Две звезды - автораспаковка словаря в питоне, cleaned_data - готовый к упротреблению словарик отвалидированных данных
-            news = form.save()
-            return redirect(news)
-    else:
-        form = NewsForm()
-    return render(request, 'news/add_news.html', {'form': form})
+# def add_news(request):
+#     if request.method == 'POST':
+#         form = NewsForm(request.POST, error_class=DivErrorList)
+#         if form.is_valid():
+#             # print(form.cleaned_data)
+#             # news = News.objects.create(**form.cleaned_data) #Две звезды - автораспаковка словаря в питоне, cleaned_data - готовый к упротреблению словарик отвалидированных данных
+#             news = form.save()
+#             return redirect(news)
+#     else:
+#         form = NewsForm()
+#     return render(request, 'news/add_news.html', {'form': form})
