17391903673f4e33f1dd6ca0b625520a06d55bc0
MiriamTool <G483@mail.ru>
2022-11-20 12:11:49 +0300

    Backup before remigrating the whole DB

--- mysite/news/models.py
+++ mysite/news/models.py
@@ -3,12 +3,14 @@ from django.urls import reverse
 
 class News(models.Model):
     title = models.CharField(max_length=150, verbose_name='Наименование')
+    # slug = models.SlugField(max_length=150, unique=True, db_index=True, verbose_name='URL')
     content = models.TextField(blank=True, verbose_name='Контент')
     created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата публикации')
     updated_at = models.DateTimeField(auto_now=True, verbose_name='Обновлено')
     photo = models.ImageField(upload_to='photos/%Y/%m/%d/', verbose_name='Фото', blank=True)
     is_published = models.BooleanField(default=True, verbose_name='Опубликовано')
     category = models.ForeignKey('Category', on_delete=models.PROTECT, verbose_name='Категория')
+    views = models.IntegerField(default=0, verbose_name='Просмотры')
 
     def my_func(self):
         return 'Hello from model'
@@ -27,6 +29,8 @@ class News(models.Model):
 
 class Category(models.Model):
     title = models.CharField(max_length=150, db_index=True, verbose_name='Наименование категории')
+    # slug = models.SlugField(max_length=150, unique=True, db_index=True,
+    #                       verbose_name='URL')
 
     def get_absolute_url(self):
         return reverse('category', kwargs={"category_id": self.pk}) #kwargs - именованные аргументы для построения данного маршрута. Метод нужен для автосоздания ссылки, аналог тега url для пайтон-файлов
